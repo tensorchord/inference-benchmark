1d59adf37c186036763dd5321a54069d2dd2692d
Zichen <liuzc@sea.com>
2023-06-21 15:07:29 +0800

    add MLServer
    Signed-off-by: Zichen <liuzc@sea.com>

---
+++ src/stable_diffusion/mlserver/client.py
@@ -0,0 +1,25 @@
+from threading import Thread
+
+import requests
+from mlserver.codecs import StringCodec
+from mlserver.types import InferenceRequest
+
+endpoint = "http://localhost:8080/v2/models/stable-diffusion/infer"
+
+inference_request_dict = InferenceRequest(
+    inputs=[
+        StringCodec.encode_input(
+            name="prompts", payload=["a happy boy with his toys"], use_bytes=False
+        )
+    ]
+).dict()
+
+
+def post():
+    response = requests.post(endpoint, json=inference_request_dict)
+    print(response.json())
+
+
+for _ in range(10):
+    # Test adaptive batching.
+    Thread(target=post).start()

09ed57dc2765a681efa5e7956096d24a7e75d37d
Keming <kemingyang@tensorchord.ai>
2023-06-22 10:25:57 +0800

    fix config
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- src/triton_services/client.py
+++ src/triton_services/client.py
@@ -1,9 +1,9 @@
 import tritonclient.http as httpclient
+import numpy as np
 
 
 with httpclient.InferenceServerClient("localhost:8000") as client:
-    inputs = [
-        httpclient.InferInput("PROMPT", shape=(1,), datatype="BYTES"),
-    ]
-    images = httpclient.InferRequestedOutput("IMAGE", binary_data=True)
-    client.infer(model_name="stable_diffusion", inputs=inputs, outputs=[images])
+    prompt = httpclient.InferInput("PROMPT", shape=(1,), datatype="BYTES"),
+    prompt.set_data_from_numpy(np.asarray(["cat"]))
+    images = httpclient.InferRequestedOutput("IMAGE", binary_data=False)
+    client.infer(model_name="stable_diffusion", inputs=[prompt], outputs=[images])

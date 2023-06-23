57d94834c02215c140b3a315a19a9866287917cd
Keming <kemingy94@gmail.com>
2023-06-23 18:21:55 +0800

    feat: add triton inference server stable diffusion (#12)
    * fix dockerfile conda checksum
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * add triton sd
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * rearrange dir
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * rebase
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * fix the dim
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * fix config
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * add timeout
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * disable dynamic batch
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * fix client req
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * fix type
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    ---------
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

---
+++ src/triton_services/stable_diffusion/1/model.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+import triton_python_backend_utils as triton_utils
+from diffusers import StableDiffusionPipeline
+
+
+class TritonPythonModel:
+    def initialize(self, args):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5",
+            torch_dtype=torch.float16 if self.device == "cuda" else torch.float32,
+        )
+        self.model.to(self.device)
+
+    def execute(self, requests):
+        responses = []
+        prompts = []
+        for request in requests:
+            prompt = (
+                triton_utils.get_input_tensor_by_name(request, "PROMPT")
+                .as_numpy()
+                .tolist()
+            )
+            prompts.append(prompt[0].decode())
+
+        images = self.model(prompts).images
+        for image in images:
+            output = triton_utils.Tensor("IMAGE", np.asarray(image))
+            resp = triton_utils.InferenceResponse(output_tensors=[output])
+            responses.append(resp)
+
+        return responses
